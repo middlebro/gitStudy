767e36a7b96579dfb0d3163df0dfdc6fc35c976d
middlebro <middlebro@inha.edu>
2018-05-25 17:40:21 +0900

    Initial commit

---
+++ simpleIO_001.cpp
@@ -0,0 +1,15 @@
+//
+//  simpleIO_001.cpp
+//  simpleIO
+//
+//  Created by 서형중 on 2018. 5. 25..
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A(3), B(2)
+    cout << A - B <<endl;
+    return 0;
+}
